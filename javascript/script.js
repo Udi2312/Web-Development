6248663b2d8a5a895971a422bfda69ff60c3a76d
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2024-09-03 11:25:45 -0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+alert("Hello World");
+
+console.log("Code is running...");
+
+var a = prompt("Enter your number");
+var istrue = confirm("Are you sure you want to leave");
+if(istrue){
+    console.log("Leaving the site...");
+}
+else{
+    console.log("Staying...")
+}
+console.log("Your number is " + a);
+
+document.title = "Udit"
+document.body.style.backgroundColor = "red";
